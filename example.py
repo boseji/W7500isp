27ad9f4b98cc64ee950209d9944a8d2c77f97b25
boseji <boseji@users.noreply.github.com>
2024-05-30 20:53:41 +0530

    Making example command line parsing oriented

--- example.py
+++ example.py
@@ -18,31 +18,70 @@
 # Imports
 from W7500isp import ispcmd
 import serial
+import argparse
+from pathlib import Path
 
+# Get the Commandline definition
+parser = argparse.ArgumentParser(
+        prog="example.py",
+        description="Surf5 Programming example")
+# Add Arguments
+parser.add_argument("-p", "--port", type=str,
+                    help="Serial Port name [/dev/ttyUSB0] (eg. COM1,COM5 or /dev/ttyUSB0, /dev/ttyACM1 etc.)",
+                    default='/dev/ttyUSB0')
+parser.add_argument("-b", "--baud", type=int,
+                    help="Serial BAUD rate [460800] (eg. 57600 or 115200 etc.)",
+                    default='460800')
+parser.add_argument("file", help="Binary File to be flashed")
+
+args = parser.parse_args()
 ###
 # Parameters
 PORT='/dev/ttyUSB0'
-BAUD=460800
-FNAME='firmware.bin'
+if args.port:
+    PORT=str(args.port)
 
+BAUD=460800
+if args.baud:
+    BAUD=int(args.baud)
+
+FNAME='firmware.bin'
+if args.file:
+    FNAME=str(args.file)
+    
+# Check the File
+target_dir = Path(FNAME).resolve()
+if not target_dir.exists():
+    print("Error the file supplied does not exist")
+    raise SystemExit(1)
+
+# Check Serial Port
+target_dir = Path(PORT)
+if not target_dir.exists():
+    print("Error the Serial Port supplied does not exists")
+    raise SystemExit(1)
 ###
 # Main Program
-print('Starting comm on ' + PORT + '@ ' + str(BAUD))
+print('Starting comm on ' + PORT + ' @ ' + str(BAUD))
+print(FNAME)
 
 # Added Timeout for faster Eject in case the boot fails
 comport = serial.Serial(PORT, BAUD, timeout=1)
 isp = ispcmd.ispcmd(comport)
 
-# Check is the Boot mode is ready
+# Check if the Boot mode is ready
+print("\n\n * Checking for Boot Mode :\n")
+print(" (Press the Boot and Reset together, keeping the Boot held release reset)")
+print(" (Release boot also once you see the 'Boot Mode Entered' message)\n") 
 isp.checkisp()
 
-print(' Lock Flags:')
-print(isp.readLockFlag())
+print('\n * Lock Flags: \n')
+isp.readLockFlag()
 
-print(' Erase All Flash:')
+print('\n * Erase All Flash: \n')
 isp.eraseFlashAll()
 
-print(' Programming Flash with ' + FNAME + '\n');
+print('\n * Programming Flash with ' + FNAME + '\n');
 isp.downloadDataByXModem( FNAME, 'code' )
 
 # Reset the MCU to start the Program
